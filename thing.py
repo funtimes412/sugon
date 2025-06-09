01728e65523e1528fb8af71213ec2dd22d0544f7
funtimes412 <77475690+funtimes412@users.noreply.github.com>
2025-06-09 23:21:12 +0800

    Update thing.py

--- thing.py
+++ thing.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def result():
     try:
-        print(request.form['test'])
+        return request.form['test']
     except Exception as e:
-        print(e)
+        return e
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8000,debug=True)
