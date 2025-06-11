c64b0848f7168f3080e5614d0c9cf43b28227701
funtimes412 <77475690+funtimes412@users.noreply.github.com>
2025-06-11 12:10:37 +0800

    Update thing.py

--- thing.py
+++ thing.py
@@ -2,17 +2,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def receive_data():
-    if request.is_json:
-        data = request.get_json() 
-        print(f"Received JSON data: {data}")
-
-        if 'test' in data: 
-            print(f"Value of 'test' key: {data['test']}")
-        else:
-            print("JSON data does not contain a 'test' key.")
-
+@app.route('/post', methods=['POST'])
+def result():
+    print(request.form['test'])
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8000,debug=True)
 #a
