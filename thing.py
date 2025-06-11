84118bc79a4d4e94b20b44f6017ceb00df6163a8
funtimes412 <77475690+funtimes412@users.noreply.github.com>
2025-06-11 12:26:42 +0800

    Update thing.py

--- thing.py
+++ thing.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/post', methods=['POST'])
+@app.route('/', methods=['POST'])
 def result():
     print(request.form['test'])
     
