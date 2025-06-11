8cef48af9f6cc8ca8d1f904770221ca851c3dfe2
funtimes412 <77475690+funtimes412@users.noreply.github.com>
2025-06-11 11:44:14 +0800

    Update thing.py

--- thing.py
+++ thing.py
@@ -2,12 +2,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def receive_data():
-    stuff = request.form["test"]
-    print(stuff)
-    return "a"
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8000,debug=True)
 #a
