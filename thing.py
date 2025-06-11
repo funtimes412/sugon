d0bd7d7bdf30eafac7c3b699be0b23a85c94e13e
funtimes412 <77475690+funtimes412@users.noreply.github.com>
2025-06-11 12:32:08 +0800

    Update thing.py

--- thing.py
+++ thing.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def result():
     print(request.form['test'])
-    return "received"
+    return request.form["test"]
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8000,debug=True)
